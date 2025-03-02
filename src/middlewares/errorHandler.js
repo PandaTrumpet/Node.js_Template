ba08a71680321c1a6f8dc67eb8d692620abcc2e4
PandaTrumpet <dsavontrumpet@gmail.com>
2025-03-02 15:40:40 +0100

    update

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,19 +1,27 @@
 import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
-  // Перевірка, чи отримали ми помилку від createHttpError
+  console.log(err);
+
+  if (err.name === 'CastError') {
+    res.status(400).json({
+      status: 400,
+      message: 'Invalid ID format',
+      error: err.message,
+    });
+    return;
+  }
+
   if (err instanceof HttpError) {
     res.status(err.status).json({
       status: err.status,
-      message: err.name,
-      data: err,
+      message: err.message,
     });
     return;
   }
 
   res.status(500).json({
-    status: 500,
     message: 'Something went wrong',
-    data: err.message,
+    error: err.message,
   });
 };
