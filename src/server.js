5fa12a01e58f215e1a493d1d0dee90deb757ecf4
PandaTrumpet <dsavontrumpet@gmail.com>
2025-03-02 15:35:32 +0100

    update

--- src/server.js
+++ src/server.js
@@ -16,17 +16,17 @@ export const startServer = () => {
     res.json({ message: 'Hello  world' });
   });
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Route not found!',
-    });
-  });
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong!',
-      error: err.message,
-    });
-  });
+  // app.use('*', (req, res, next) => {
+  //   res.status(404).json({
+  //     message: 'Route not found!',
+  //   });
+  // });
+  // app.use((err, req, res, next) => {
+  //   res.status(500).json({
+  //     message: 'Something went wrong!',
+  //     error: err.message,
+  //   });
+  // });
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
